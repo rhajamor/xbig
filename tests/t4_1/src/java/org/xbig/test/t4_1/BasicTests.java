c3c9a6ecf2ab05066aa3185222f657ba9da4367e
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2006-11-21 12:43:39 +0000

    added working junit tests

---
+++ tests/t4_1/src/java/org/xbig/test/t4_1/BasicTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.xbig.test.t4_1;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xbig.n.A;
+/**
+ * @author bielig
+ *
+ */
+public class BasicTests {
+	
+	@Test
+	public void print() {
+		A a = new A();
+	   int b = a.a((float)7.3);
+	   Assert.assertEquals(7, b);
+	}
+}
