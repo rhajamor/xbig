5d6f1a48c91ce6e312a3e3e1ac8412d98e501497
yavin02 <yavin02@1b918d8c-a676-11de-be8d-735280eae207>
2007-02-01 16:14:56 +0000

    fixed wrong commit

--- trunk/tests/t12_1/src/java/org/xbig/test/t12_1/BasicTests.java
+++ trunk/tests/t12_1/src/java/org/xbig/test/t12_1/BasicTests.java
@@ -5,8 +5,7 @@ package org.xbig.test.t12_1;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.xbig.base.IntArray;
-import org.xbig.base.AArray;
+import org.xbig.base.NativeObjectPointer;
 import org.xbig.ns1.A;
 
 /**
@@ -18,37 +17,12 @@ public class BasicTests {
 	@Test
 	public void test1() {
 	   A a = new A();       
+       NativeObjectPointer<A> aa = a.getPointerPointer();
        
-       IntArray intArray = new IntArray(5);
-       intArray.set(0,1);
-       intArray.set(1,2);
-       intArray.set(2,3);
-       intArray.set(3,4);
-       intArray.set(4,5);
-       a.setIntArray(intArray);
+       A b = new A();
+       aa.getObject(b);
        
-       Assert.assertEquals(1, intArray.get(0));
-       Assert.assertEquals(2, intArray.get(1));
-       Assert.assertEquals(3, intArray.get(2));
-       Assert.assertEquals(4, intArray.get(3));
-       Assert.assertEquals(5, intArray.get(4));
-       
-       AArray aArray = new AArray(3);
-       aArray.set(0,new A());
-       aArray.set(1,new A());
-       aArray.set(2,new A());
-       aArray.set(3,new A());
-       aArray.set(4,new A());
-       a.setAArray(aArray);
-       
-       aArray.get(0).delete();
-       aArray.get(1).delete();
-       aArray.get(2).delete();
-       aArray.get(3).delete();
-       aArray.get(4).delete();
-
-       aArray.delete();
-       intArray.delete();
        a.delete();
+       b.delete();
 	}
 }
