bfff5e05c49ca4311be62d0a1fe9dc22afd0a17b
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-02-08 16:27:08 +0000

    extended stl wrapper and added a test for it

---
+++ trunk/tests/t13_4/include/t13_4.h
@@ -0,0 +1,41 @@
+/******************************************************************
+ *
+ * Test file for the XSLT Bindings Generator (XBiG)
+ *
+ * It tests the stl wrapper
+ *
+ ******************************************************************/
+
+#ifdef WIN32
+	#define EXPORT __declspec(dllexport)
+#else
+	#define EXPORT
+#endif
+
+#include <string>
+#include <vector>
+#include <map>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <set>
+//#include <bitset>
+
+
+class EXPORT A {
+public:
+};
+
+
+typedef std::vector<std::string>         StringVector;
+typedef std::map<std::string, A>         AMap;
+typedef std::deque<std::string>          StringDeque;
+typedef std::list<std::string>           StringList;
+typedef std::priority_queue<std::string> StringPriorityQueue;
+typedef std::queue<std::string>          StringQueue;
+typedef std::stack<std::string>          StringStack;
+typedef std::set<std::string>            StringSet;
+typedef std::multiset<std::string>       StringMultiset;
+typedef std::multimap<std::string, A>    AMultimap;
+//typedef std::bitset<unsigned int>        IntBitset;
---
+++ trunk/tests/t13_4/src/java/org/xbig/test/t13_4/BasicTests.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.xbig.test.t13_4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xbig.base.StringPointer;
+//import org.xbig.std.Ivector;
+import org.xbig.IA;
+import org.xbig.IAMap;
+import org.xbig.IAMultimap;
+import org.xbig.IStringDeque;
+import org.xbig.IStringList;
+import org.xbig.IStringMultiset;
+import org.xbig.IStringPriorityQueue;
+import org.xbig.IStringSet;
+import org.xbig.IStringStack;
+import org.xbig.IStringVector;
+import org.xbig.IA;
+import org.xbig.AMap;
+import org.xbig.AMultimap;
+import org.xbig.StringDeque;
+import org.xbig.StringList;
+import org.xbig.StringMultiset;
+import org.xbig.StringPriorityQueue;
+import org.xbig.StringSet;
+import org.xbig.StringStack;
+import org.xbig.StringVector;
+
+/**
+ * @author nenning
+ *
+ */
+public class BasicTests {
+	
+	@Test
+	public void useStlWrapper() {
+		// when generated code compiles, this test is considered successfull
+	}
+}
