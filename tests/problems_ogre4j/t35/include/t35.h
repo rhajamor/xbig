5ee3b16f0fb7ab781b3d0eb9f5ad199f797fdd00
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-02-21 17:18:42 +0000

    added a testcase for a many compile error causing problem in new ogre4j

---
+++ tests/problems_ogre4j/t35/include/t35.h
@@ -0,0 +1,94 @@
+
+
+/******************************************************************
+ * This file has been created to test
+ * the XSLT Bindings Generator (XBiG)
+ *
+ * It is based on a problem occured
+ * during the generation of ogre4j
+ *
+ * Problem:
+ * inherited type with pointer as parameter
+ ******************************************************************/
+
+
+#include <map>
+#include <string>
+
+
+namespace Ogre {
+	template <class T>
+	class MapIterator
+	{
+	private:
+		typename T::iterator mCurrent;
+		typename T::iterator mEnd;
+		MapIterator() {};
+	public:
+		typedef typename T::mapped_type MappedType;
+		typedef typename T::key_type KeyType;
+	
+		MapIterator(typename T::iterator start, typename T::iterator end)
+		: mCurrent(start), mEnd(end)
+		{
+		}
+		explicit MapIterator(T& c)
+		: mCurrent(c.begin()), mEnd(c.end())
+		{
+		}
+		bool hasMoreElements(void) const
+		{
+			return mCurrent != mEnd;
+		}
+		typename T::mapped_type getNext(void)
+		{
+			return (mCurrent++)->second;
+		}
+		typename T::mapped_type peekNextValue(void)
+		{
+			return mCurrent->second;
+		}
+		typename T::key_type peekNextKey(void)
+		{
+			return mCurrent->first;
+		}
+		MapIterator<T> & operator=( MapIterator<T> &rhs )
+		{
+			mCurrent = rhs.mCurrent;
+			mEnd = rhs.mEnd;
+			return *this;
+		}
+		typename T::mapped_type* peekNextValuePtr(void)
+		{
+			return &(mCurrent->second);
+		}
+		void moveNext(void)
+		{
+			++mCurrent;
+		}
+	};
+
+	typedef std::string String;
+
+	class Animation {
+	};
+
+	class SceneManager {
+	//protected:
+	public:
+		typedef std::map<String, Animation*> AnimationList;
+
+	public:
+		typedef MapIterator<AnimationList> AnimationIterator;
+		virtual const String & getTypeName (void) const =0;
+	};
+
+	class DefaultSceneManager : public SceneManager {
+	public:
+		AnimationIterator getAnimationIterator() {AnimationIterator a(m); return a;}
+		const String & getTypeName (void) const {return "Bulldozer";}
+
+	private:
+		AnimationList m;
+	};
+}
---
+++ tests/problems_ogre4j/t35/src/test/simple.cpp
@@ -0,0 +1,6 @@
+
+#include "t35.h"
+
+int main(int argc, char* argv[]) 
+{
+}
