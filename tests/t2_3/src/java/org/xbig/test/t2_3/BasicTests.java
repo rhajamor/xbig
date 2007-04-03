e7ed20f97defc0d3265257ec59997723d1abe422
geomap <geomap@1b918d8c-a676-11de-be8d-735280eae207>
2007-04-03 09:43:02 +0000

    XSLTdoc integrated

--- tests/t2_3/src/java/org/xbig/test/t2_3/BasicTests.java
+++ tests/t2_3/src/java/org/xbig/test/t2_3/BasicTests.java
@@ -8,15 +8,55 @@ import org.junit.Test;
 import org.xbig.A;
 /**
  * @author nenning
- *
+ * 
  */
 public class BasicTests {
 	
 	@Test
-	public void callMethod() {
+	public void callMethodWithDifferentNamesDifferentParameters() {
 		A a = new A();
-		int b = a.a((float)7.3);
-		Assert.assertEquals(7, b);
+        
+        byte  byteValue = 13;
+        short smallShortValue = 234;
+        short shortValue = 22513;
+        int  intValue = 103455;
+        long longValue = 32424567;
+        float floatValue = 123.234f;
+        double doubleValue = 12314.563456;
+        
+        Assert.assertEquals(a.getChar('c'),'c');
+        Assert.assertEquals(a.getUnsignedChar(smallByteValue), smallByteValue);
+        Assert.assertEquals(a.getSignedChar(byteValue), byteValue);
+        
+        Assert.assertEquals(a.getShort(shortValue), shortValue);
+        Assert.assertEquals(a.getUnsignedShort(intValue), intValue);
+
+        Assert.assertEquals(a.getInt(intValue), intValue);
+        Assert.assertEquals(a.getUnsignedInt(longValue), longValue);
+
+        Assert.assertEquals(a.getLong(longValue), longValue);
+        Assert.assertEquals(a.getUnsignedLong(longValue), longValue);
+        
+        Assert.assertEquals(a.getFloat(floatValue), floatValue);
+        Assert.assertEquals(a.getDouble(doubleValue), doubleValue);
+        
 		a.delete();
 	}
+    
+    @Test
+    public void callMethodWithSameNameDifferentParameters() {
+        A a = new A();
+        
+        byte  byteValue = 13;
+        short smallShortValue = 234;
+        short shortValue = 22513;
+        int  intValue = 103455;
+        long longValue = 32424567;
+        float floatValue = 123.234f;
+        double doubleValue = 12314.563456;
+        
+        
+        a.delete();
+    }
+    
 }
