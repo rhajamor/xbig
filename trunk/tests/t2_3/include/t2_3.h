7db96d5b0249b64538f7a9f0d1e18395f8813448
ohhappyshen <ohhappyshen@1b918d8c-a676-11de-be8d-735280eae207>
2007-05-04 09:06:06 +0000

    complete basic data type mapping. "unsigned long long" is not included.

--- trunk/tests/t2_3/include/t2_3.h
+++ trunk/tests/t2_3/include/t2_3.h
@@ -15,18 +15,36 @@
 class EXPORT A {
 public:
 	A();
+		bool getBool(bool v);
         char getChar(char v);
         unsigned char getUnsignedChar(unsigned char v);
         signed char getSignedChar(signed char v);
         short getShort(short v);
+        short int getShortInt(short int v);
+        signed short getSignedShort(signed short v);
+        signed short int getSignedShortInt(signed short int v);
         unsigned short getUnsignedShort(unsigned short v);
+        unsigned short int getUnsignedShortInt(unsigned short int v);
         int getInt(int v);
+        signed getSigned(signed v);
+        signed int getSignedInt(signed int v);
+        unsigned getUnsigned(unsigned v);
         unsigned int getUnsignedInt(unsigned int v);
         long getLong(long v);
+        long int getLongInt(long int v);
+        signed long getSignedLong(signed long v);
+        signed long int getSignedLongInt(signed long int v);        
         unsigned long getUnsignedLong(unsigned long v);
+        unsigned long int getUnsignedLongInt(unsigned long int v);
+        long long getLongLong(long long v);
+        signed long long getSignedLongLong(signed long long v);
+        //unsigned long long getUnsignedLongLong(unsigned long long v);
         float getFloat(float v);
-        double getDouble(double v);   
+        double getDouble(double v);
 };
+
+/*
+Move B to t2_9
 
 class EXPORT B {
 public:
@@ -42,5 +60,5 @@ public:
         unsigned long getUnsigned(unsigned short v);
         unsigned long getUnsigned(unsigned int v);
         unsigned long getUnsigned(unsigned long v);                
-};
+};*/
 
