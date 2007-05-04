df255856aa4928c5aaec54ad4dede845fec121fb
ohhappyshen <ohhappyshen@1b918d8c-a676-11de-be8d-735280eae207>
2007-05-04 09:06:58 +0000

    complete basic data type mapping. "unsigned long long" is not included.

--- trunk/tests/t2_3/src/java/org/xbig/test/t2_3/BasicTests.java
+++ trunk/tests/t2_3/src/java/org/xbig/test/t2_3/BasicTests.java
@@ -3,6 +3,8 @@
  */
 package org.xbig.test.t2_3;
 
+
+import java.math.BigInteger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.xbig.A;
@@ -16,33 +18,68 @@ public class BasicTests {
 	public void callMethodWithDifferentNamesDifferentParameters() {
 		A a = new A();
         
-        byte  byteValue = 13;
-        short smallShortValue = 234;
+		short charValue = 127;
+        byte  signedCharValue = -128;
+        short unsignedCharValue = 234;
+        Assert.assertEquals(a.getChar(charValue),charValue);
+        Assert.assertEquals(a.getUnsignedChar(unsignedCharValue), unsignedCharValue);
+        Assert.assertEquals(a.getSignedChar(signedCharValue), signedCharValue);
+        
         short shortValue = 22513;
-        int  intValue = 103455;
-        long longValue = 32424567;
+        short shortIntValue = -22513;
+        short signedShortValue = 32767;
+        short signedShortIntValue = -32768;
+        int unsignedShortValue = 50000;
+        int unsignedShortIntValue = 65535;
+        Assert.assertEquals(a.getShort(shortValue), shortValue);
+        Assert.assertEquals(a.getShortInt(shortIntValue), shortIntValue);
+        Assert.assertEquals(a.getSignedShort(signedShortValue), signedShortValue);
+        Assert.assertEquals(a.getSignedShortInt(signedShortIntValue), signedShortIntValue);
+        Assert.assertEquals(a.getUnsignedShort(unsignedShortValue), unsignedShortValue);
+        Assert.assertEquals(a.getUnsignedShortInt(unsignedShortIntValue), unsignedShortIntValue);
+        
+        int intValue = 2147483647;
+        int signedValue = -2147483648;
+        int signedIntValue = 2000000000;
+        long unsignedValue = 3000000000L;
+        long unsignedIntValue = 4294967295L;
+        Assert.assertEquals(a.getInt(intValue), intValue);
+        Assert.assertEquals(a.getSigned(signedValue), signedValue);
+        Assert.assertEquals(a.getSignedInt(signedIntValue), signedIntValue);      
+        Assert.assertEquals(a.getUnsigned(unsignedValue), unsignedValue);
+        Assert.assertEquals(a.getUnsignedInt(unsignedIntValue), unsignedIntValue);
+        
+        long longValue = 1000000000L;
+        long longIntValue = 2147483647L;
+        long signedLongValue = -1000000000L;
+        long signedLongIntValue = -2147483648L;
+        long unsignedLongValue = 3000000000L;
+        long unsignedLongIntValue = 4294967295L;
+        Assert.assertEquals(a.getLong(longValue), longValue);
+        Assert.assertEquals(a.getLongInt(longIntValue), longIntValue);
+        Assert.assertEquals(a.getSignedLong(signedLongValue), signedLongValue);
+        Assert.assertEquals(a.getSignedLongInt(signedLongIntValue), signedLongIntValue);
+        Assert.assertEquals(a.getUnsignedLong(unsignedLongValue), unsignedLongValue);
+        Assert.assertEquals(a.getUnsignedLongInt(unsignedLongIntValue), unsignedLongIntValue);
+
+        long longLongValue = -9223372036854775808L;
+        long signedLongLongValue = 9223372036854775807L;
+        //BigInteger unsignedLongLongValue = new BigInteger("18446744073709551615");
+        Assert.assertEquals(a.getLongLong(longLongValue), longLongValue);
+        Assert.assertEquals(a.getSignedLongLong(signedLongLongValue), signedLongLongValue);
+        //Assert.assertEquals(a.getUnsignedLongLong(unsignedLongLongValue), unsignedLongLongValue);
+
         float floatValue = 123.234f;
         double doubleValue = 12314.563456;
-        
-        Assert.assertEquals(a.getChar('c'),'c');
-        Assert.assertEquals(a.getUnsignedChar(smallByteValue), smallByteValue);
-        Assert.assertEquals(a.getSignedChar(byteValue), byteValue);
-        
-        Assert.assertEquals(a.getShort(shortValue), shortValue);
-        Assert.assertEquals(a.getUnsignedShort(intValue), intValue);
-
-        Assert.assertEquals(a.getInt(intValue), intValue);
-        Assert.assertEquals(a.getUnsignedInt(longValue), longValue);
-
-        Assert.assertEquals(a.getLong(longValue), longValue);
-        Assert.assertEquals(a.getUnsignedLong(longValue), longValue);
-        
         Assert.assertEquals(a.getFloat(floatValue), floatValue);
         Assert.assertEquals(a.getDouble(doubleValue), doubleValue);
         
 		a.delete();
 	}
     
+	/*
+	 Move B to t2_9
+	 
     @Test
     public void callMethodWithSameNameDifferentParameters() {
         A a = new A();
@@ -57,6 +94,6 @@ public class BasicTests {
         
         
         a.delete();
-    }
+    }*/
     
 }
