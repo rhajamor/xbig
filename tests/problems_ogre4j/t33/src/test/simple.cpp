2d56e5061a8b131a08d47f76967361cdd1e16ed9
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-05-11 13:49:44 +0000

    improved t33 and t35 a little

--- tests/problems_ogre4j/t33/src/test/simple.cpp
+++ tests/problems_ogre4j/t33/src/test/simple.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 
 int main(int argc, char* argv[]) 
-{
+{/*
    RadixSort<std::vector<int>, int, long>::ContainerIter a;
 
    std::cout << "base: " << a.base() << std::endl;
 
    std::cout << "done" << std::endl;
-}
+*/}
--- tests/problems_ogre4j/t35/src/lib/t35.cpp
+++ tests/problems_ogre4j/t35/src/lib/t35.cpp
@@ -1,3 +1,25 @@
 
 
 #include "t35.h"
+
+/*
+Ogre::DefaultSceneManager::DefaultSceneManager() {
+	str = "Bulldozer";
+	ai = new AnimationIterator(m);
+	cai = new ConstAnimationIterator(mcal);
+	capi = new ConstAnimationPointerIterator(mcapl);
+}
+
+Ogre::DefaultSceneManager::DefaultSceneManager(const DefaultSceneManager& dsm) {
+	str = "Bulldozer";
+	ai = new AnimationIterator(m);
+	cai = new ConstAnimationIterator(mcal);
+	capi = new ConstAnimationPointerIterator(mcapl);
+}
+
+Ogre::DefaultSceneManager::~DefaultSceneManager() {
+	delete ai;
+	delete cai;
+	delete capi;
+}
+*/
