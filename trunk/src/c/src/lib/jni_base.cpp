4c3990c5d29223b4dc696a638d2d639b8e1c9021
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-05-24 13:35:11 +0000

    fixed a problem with char* on Windows, see bug 1685719

--- trunk/src/c/src/lib/jni_base.cpp
+++ trunk/src/c/src/lib/jni_base.cpp
@@ -1,8 +1,15 @@
 #include <jni_base.h>
 
+#include <iostream>
+
 std::string org::xbig::jni::to_stdstring(JNIEnv* env, jstring jString) {
 	const char* c_str = env->GetStringUTFChars(jString, JNI_FALSE); // false: create a copy
 	return std::string(c_str);
+}
+
+char* org::xbig::jni::to_cstring(JNIEnv* env, jstring jString) {
+	char* c_str = (char*)env->GetStringUTFChars(jString, JNI_FALSE); // false: create a copy
+	return c_str;
 }
 
 
