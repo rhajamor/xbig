056e6d433306b3ff3990ceb561cca250bd963bfa
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-05-30 10:24:09 +0000

    fixed bug 1724318. added t49. interfaces for inner types of interfaces for templates do not get any methods, too.

---
+++ trunk/tests/problems_ogre4j/t49/src/java/org/xbig/test/t49/BasicTests.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.xbig.test.t49;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xbig.Ogre.IRadixSort;
+/**
+ * @author nenning
+ *
+ */
+public class BasicTests {
+	
+	@Test
+	public void test() {
+        IRadixSort rs = null;
+        IRadixSort.ISortEntry se = null;
+	}
+}
---
+++ trunk/tests/problems_ogre4j/t49/src/test/simple.cpp
@@ -0,0 +1,6 @@
+
+#include "t49.h"
+
+int main(int argc, char* argv[]) 
+{
+}
