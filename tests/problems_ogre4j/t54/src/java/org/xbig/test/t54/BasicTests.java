2e9fca091610ebac908f34e554a0e1f612c19859
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-06-06 13:44:13 +0000

    added t54 to handle bug 1728982

---
+++ tests/problems_ogre4j/t54/src/java/org/xbig/test/t54/BasicTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.xbig.test.t54;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xbig.Ogre.WireBoundingBox;
+/**
+ * @author nenning
+ *
+ */
+public class BasicTests {
+
+	@Test
+	public void test() {
+        WireBoundingBox wbb = new WireBoundingBox();
+
+        wbb.delete();
+    }
+}
---
+++ tests/problems_ogre4j/t54/src/test/simple.cpp
@@ -0,0 +1,6 @@
+
+#include "t54.h"
+
+int main(int argc, char* argv[]) 
+{
+}
