94da5bfc7696e2099630467ccd228e6d0b2cec16
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2007-06-08 15:11:43 +0000

    added tests t59 and t60 for bugs 1728995 and 1729000

---
+++ trunk/tests/problems_ogre4j/t59/src/test/simple.cpp
@@ -0,0 +1,6 @@
+
+#include "t59.h"
+
+int main(int argc, char* argv[]) 
+{
+}
---
+++ trunk/tests/problems_ogre4j/t60/include/t60.h
@@ -0,0 +1,47 @@
+
+#ifndef __T59_H__
+#define __T59_H__
+
+/******************************************************************
+ * This file has been created to test
+ * the XSLT Bindings Generator (XBiG)
+ *
+ * It is based on a problem occured
+ * during the generation of ogre4j
+ *
+ * Problem:
+ * method does not exist in original lib, see bug 1729000
+ ******************************************************************/
+
+#ifdef WIN32
+	#define EXPORT __declspec(dllexport)
+#else
+	#define EXPORT
+#endif
+
+
+#include <vector>
+
+
+namespace Ogre {
+
+
+	class EXPORT Light {};
+	typedef std::vector<Light*> LightList;
+
+	class EXPORT MovableObject {
+		LightList mLights;
+	public:
+		virtual LightList& queryLights (void) /*const*/ {return mLights;}
+
+		class EXPORT Listener {
+			LightList mLights;
+		public:
+			virtual const LightList* objectQueryLights(const MovableObject *) {return &mLights;}
+		};
+	};
+
+	class EXPORT RibbonTrail : public MovableObject {};
+}
+
+#endif
