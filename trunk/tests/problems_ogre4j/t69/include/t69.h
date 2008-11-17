84943fe8362590f747e97e4b115359aaabd6cfe6
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2008-11-17 11:51:05 +0000

    upgraded test case 69

--- trunk/tests/problems_ogre4j/t69/include/t69.h
+++ trunk/tests/problems_ogre4j/t69/include/t69.h
@@ -52,6 +52,40 @@ namespace Ogre {
 
 	};
 
+	#define OGRE_SIMD_ALIGNMENT  16
+
+	class EXPORT AlignedMemory
+	{
+	public:
+        static void* allocate(size_t size, size_t alignment)
+        {
+			//assert(0 < alignment && alignment <= 128 && Bitwise::isPO2(alignment));
+
+			unsigned char* p = new unsigned char[size + alignment];
+			size_t offset = alignment - (size_t(p) & (alignment-1));
+
+			unsigned char* result = p + offset;
+			result[-1] = (unsigned char)offset;
+
+			return result;
+		}
+
+        static void* allocate(size_t size)
+		{
+			return allocate(size, OGRE_SIMD_ALIGNMENT);
+		}
+
+        static void deallocate(void* p)
+        {
+            if (p)
+            {
+                unsigned char* mem = (unsigned char*)p;
+                mem = mem - mem[-1];
+                delete [] mem;
+            }
+        }
+	};
+
 	template <size_t Alignment = 0>
 	class StdAlignedAllocPolicy
 	{
