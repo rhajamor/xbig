553a4cc36e1f8db73e526e83b39d8e51b766349d
kex347 <kex347@1b918d8c-a676-11de-be8d-735280eae207>
2008-11-18 10:35:32 +0000

    updated windows run-all-tests-scripts for ant 1.7

--- trunk/tests/build.bat
+++ trunk/tests/build.bat
@@ -1,3 +1,3 @@
 @ECHO off
 ECHO Building Testsuite ... > build.log
-FOR /D %%G IN (t*) DO (ECHO ----- %%G ----- >> build.log & cd %%G & ant -lib ..\..\src\ant\jar clean > ..\tmp.txt & ant -lib ..\..\src\ant\jar >> ..\build.log 2>&1 & cd .. & del tmp.txt)
+FOR /D %%G IN (t*) DO (ECHO ----- %%G ----- >> build.log & cd %%G & ant clean > ..\tmp.txt & ant >> ..\build.log 2>&1 & cd .. & del tmp.txt)
