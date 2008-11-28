709e020a7c9cd3ce27e378ebb7120472cace0d01
bosch_benjamin <bosch_benjamin@1b918d8c-a676-11de-be8d-735280eae207>
2008-11-28 12:55:48 +0000

    added native buffers for different primitive types

--- trunk/tests/org.xbig.base/t01/include/t01.h
+++ trunk/tests/org.xbig.base/t01/include/t01.h
@@ -2,7 +2,7 @@
  *
  * Test file for the XSLT Bindings Generator (XBiG)
  *
- * It handles org.xbig.base.NativeBuffer.
+ * It handles org.xbig.base.Native...Buffer.
  *
  ******************************************************************/
 
@@ -15,9 +15,57 @@
 class EXPORT A
 {
 public:
-	void fillBuffer(void* buffer)
+	void fillByteBuffer(void* buffer)
 	{
 		unsigned char* castedBuffer = (unsigned char*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillCharBuffer(void* buffer)
+	{
+		char* castedBuffer = (char*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillShortBuffer(void* buffer)
+	{
+		short* castedBuffer = (short*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillIntBuffer(void* buffer)
+	{
+		int* castedBuffer = (int*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillLongBuffer(void* buffer)
+	{
+		long* castedBuffer = (long*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillFloatBuffer(void* buffer)
+	{
+		float* castedBuffer = (float*)buffer;
+		for (int i=0; i<5; ++i) {
+			castedBuffer[i] = i;
+		}
+	}
+
+	void fillDoubleBuffer(void* buffer)
+	{
+		double* castedBuffer = (double*)buffer;
 		for (int i=0; i<5; ++i) {
 			castedBuffer[i] = i;
 		}
